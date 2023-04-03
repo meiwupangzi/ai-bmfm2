9232bbc8888da03711d4c144e3d3e3e4e8e1e32a
Tomda <67380286+Tomccc520@users.noreply.github.com>
2023-04-03 21:33:07 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -11,11 +11,6 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `Powered by OpenAI Vercel
-- 如果本项目对你有所帮助，可以关注公众号 [Tomda]但不接受任何付费功能请求。
-- 本网站仅作为项目演示，不提供长期服务，网站密码为 \`ourongxing\`，请勿滥用，长期使用请 [自行部署](https://github.com/Tomccc520/chatgpt-tomda)，简单成本低。
-- [个人网站](https://www.tomda.top/)
--![](./assets/tomda.png)
-- API Key 自己填写自己使用，本站只做展示。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof defaultSetting
